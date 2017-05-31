9e6ebe90d45866f2bac377d8813833fc381a00d2
Kynosaur <kye.bracey@gmail.com>
2017-05-31 11:53:31 +0100

    Rewrite feature test for links

--- spec/features/list_of_links_spec.rb
+++ spec/features/list_of_links_spec.rb
@@ -1,7 +1,9 @@
 feature 'See a list of links, in chronological order, on a homepage' do
   scenario 'User sees a list of links' do
-    Link.create(url: "http://google.co.uk")
+    Link.create(url: 'http://google.co.uk')
     visit('/links')
-    expect(page).to have_selector("a[href='http://google.co.uk']")
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
   end
 end
