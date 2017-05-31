213fac96dec5daa493e1218fc68058c5e6e0c879
Kynosaur <kye.bracey@gmail.com>
2017-05-31 12:18:15 +0100

    Add view to render list of links

--- spec/features/list_of_links_spec.rb
+++ spec/features/list_of_links_spec.rb
@@ -1,6 +1,6 @@
 feature 'See a list of links, in chronological order, on a homepage' do
   scenario 'User sees a list of links' do
-    Link.create(url: 'http://google.co.uk')
+    Link.create(url: 'http://google.co.uk', title: 'Google')
     visit('/links')
     within 'ul#links' do
       expect(page).to have_content('Google')
